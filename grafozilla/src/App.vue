406abced50eaf527b6f6540e5e2de56c60cb75c8
dev1d123 <dhuamanio@unsa.edu.pe>
2024-09-18 05:08:33 -0500

    Changed to test

--- grafozilla/src/App.vue
+++ grafozilla/src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <!--FIRST PAGE, the welcome...-->
+  
   <MainScreen></MainScreen>
 
   <body>
     <NavBar></NavBar>
     <div>
-      <OptionsContainer></OptionsContainer>
+      <!--<OptionsContainer></OptionsContainer>-->
+      <SpecificOption></SpecificOption>
     </div>
     <FooterPage></FooterPage>
   </body>
-
+  <!--SECOND PAGE, the selector...-->
 </template>
 
 <script lang="ts">
@@ -16,14 +19,16 @@ import { defineComponent } from 'vue';
 import NavBar from './components/NavBar.vue';
 import FooterPage from './components/FooterPage.vue';
 import MainScreen from './components/MainScreen.vue';
-import OptionsContainer from './components/OptionsContainer.vue';
+//import OptionsContainer from './components/OptionsContainer.vue';
+import SpecificOption from './components/SpecificOption.vue';
 export default defineComponent({
   name: 'App',
   components: {
     NavBar,
     FooterPage,
     MainScreen,
-    OptionsContainer
+    SpecificOption,
+    //OptionsContainer
   }
 });
 </script>
